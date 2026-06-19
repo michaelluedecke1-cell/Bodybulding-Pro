0a5353e7d427237ab7436866b72c2e2cdb34042b
michaelluedecke1-cell <michael.luedecke1@googlemail.com>
2026-06-19 18:15:22 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'bb-pro-v1';
+const ASSETS = ['index.html', 'manifest.json', 'gym-123.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )));
+});
